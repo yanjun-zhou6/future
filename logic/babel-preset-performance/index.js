c3c82d265ce8c896b274f0c03e6551b941afcfab
unnkoel <unnkoel@gmail.com>
2019-08-12 14:21:38 +0800

    add razzle & bable performance plugin

---
+++ logic/babel-preset-performance/index.js
@@ -0,0 +1,40 @@
+const validateBoolOption = (name, value, defaultValue) => {
+  if (typeof value === 'undefined') {
+    value = defaultValue;
+  }
+
+  if (typeof value !== 'boolean') {
+    throw new Error (`Preset react-app: '${name}' option must be a boolean.`);
+  }
+
+  return value;
+};
+
+module.exports = function (api, opts) {
+  const env = process.env.BABEL_ENV || process.env.NODE_ENV;
+
+  var isEnvProduction = env === 'production';
+
+  var useLodash = validateBoolOption (
+    'useLodash',
+    opts.lodash,
+    isEnvProduction
+  );
+
+  var useRemovePropTypes = validateBoolOption (
+    'useRemovePropTypes',
+    opts.removePropTypes,
+    isEnvProduction
+  );
+
+  return (
+    isEnvProduction && {
+      plugins: [
+        useLodash && [require ('lodash').default],
+        useRemovePropTypes && [
+          require ('transform-react-remove-prop-types').default,
+        ],
+      ],
+    }
+  );
+};
---
+++ logic/razzle-plugin-performance/index.js
@@ -0,0 +1,48 @@
+const MomentLocalesPlugin = require ('moment-locales-webpack-plugin');
+const DuplicatePackageCheckerPlugin = require ('duplicate-package-checker-webpack-plugin');
+const BundleAnalyzerPlugin = require ('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
+
+module.exports = function myRazzlePlugin (config, env, webpack, options) {
+  const {target, dev} = env;
+
+  if (target === 'web') {
+    //code splitting @ant-design/icons
+    config.module.rules.push ({
+      loader: require.resolve ('webpack-ant-icon-loader'),
+      enforce: 'pre',
+      options: {
+        chunkName: 'antd-icons',
+      },
+      include: [require.resolve ('@ant-design/icons/lib/dist')],
+    });
+
+    config.module.rules.push ({
+      test: /\.(jpe?g|png|gif|svg)$/,
+      loader: require.resolve ('image-webpack-loader'),
+      enforce: 'pre',
+    });
+
+    if (!dev) {
+      // bundle analysis
+      config.plugins.push (
+        new BundleAnalyzerPlugin ({
+          analyzerMode: 'disabled',
+          generateStatsFile: true,
+        })
+      );
+      // identity duplicate dependencies
+      config.plugins.push (new DuplicatePackageCheckerPlugin ());
+    }
+  }
+
+  // remove moment locales extends 'zh-cn'
+  config.plugins.push (
+    new MomentLocalesPlugin ({
+      localesToKeep: ['zh-cn'],
+    })
+  );
+
+  // Do some stuff...
+  return config;
+};
